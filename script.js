58099288dd610127f4a2745982b659aab7fe2f01
Ketevan <qeto71@gmail.com>
2023-05-08 14:11:31 +0400

    update

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function gamotvla() {
     var input2 = document.getElementById("number2").value;
 
     document.getElementById("result").innerHTML = Number(input1) / Number(input2);
-
+    document.getElementById("result1").innerHTML = Number(input1) * Number(input2);
+    document.getElementById("result2").innerHTML = Number(input1) + Number(input2);
+    document.getElementById("result3").innerHTML = Number(input1) - Number(input2);
+    
     return false;
 }
